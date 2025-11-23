3aafab0fbc04d8325af668a4cdcaaf2ed2fdea33
Tobias Gacko <e11730105@tuwien.ac.at>
2025-11-23 22:43:23 +0100

    [Design] Added logo + developers + possible links

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { Neo4jPropertyGraphComponent } from './neo4j-property-graph/neo4j-property-graph.component';
 import { CustomPropertyGraphComponent } from './custom-property-graph/custom-property-graph.component';
 import { HeaderComponent } from './header/header.component';
+import { RdfComponent } from './rdf/rdf.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Neo4jPropertyGraphComponent,
     CustomPropertyGraphComponent,
-    HeaderComponent
+    HeaderComponent,
+    RdfComponent
   ],
   imports: [
     BrowserModule,
